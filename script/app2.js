2bc04a339cde6640eb935ef859c40f794253132f
lucasdriessens <lucas.driessens@student.howest.be>
2022-10-27 15:10:41 +0200

    na lightning

--- script/app2.js
+++ script/app2.js
@@ -1,2 +1,6 @@
-const animation = document.querySelector('#lightning');
-animation.beginElement();
+document
+	.querySelector('.js-button-lightning')
+	.addEventListener('click', function () {
+		var animate = document.getElementById('lightning');
+		animate.beginElement();
+	});
